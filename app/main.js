cd12884ac71216e1cd839cec8a0d40030d77c570
promise <promiseid@126.com>
2018-03-15 18:39:04 +0800

    plugins

---
+++ app/main.js
@@ -0,0 +1,8 @@
+// var LiveSearch = require('./keySearch')
+// import LiveSearch from './keySearch';
+// var liveSearch = new LiveSearch();
+
+//Quill.js DEMO
+import demoQuill from './demoQuill';
+demoQuill();
+
